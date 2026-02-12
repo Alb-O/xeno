03480d2e36e91e9dbde8172a7465c6e7ac99abd9
Albert O'Shea <albertoshea2@gmail.com>
2026-02-13 09:04:11 +1100

    editor: make overlay pane geometry math backend-agnostic

--- crates/editor/src/overlay/geom.rs
+++ crates/editor/src/overlay/geom.rs
@@ -1,17 +1,29 @@
-use xeno_tui::widgets::{Block, Borders};
-
 use crate::geometry::Rect;
 use crate::window::SurfaceStyle;
 
 pub fn pane_inner_rect(rect: Rect, style: &SurfaceStyle) -> Rect {
-	let mut block = Block::default().padding(style.padding);
-	if style.border {
-		block = block.borders(Borders::ALL).border_type(style.border_type);
-		if let Some(title) = &style.title {
-			block = block.title(title.as_str());
-		}
-	}
-	block.inner(rect.into()).into()
+	let border_left = u16::from(style.border);
+	let border_right = u16::from(style.border);
+	let border_top = u16::from(style.border);
+	let border_bottom = u16::from(style.border);
+
+	let x = rect.x.saturating_add(border_left).saturating_add(style.padding.left);
+	let y = rect.y.saturating_add(border_top).saturating_add(style.padding.top);
+	let horizontal = border_left
+		.saturating_add(border_right)
+		.saturating_add(style.padding.left)
+		.saturating_add(style.padding.right);
+	let vertical = border_top
+		.saturating_add(border_bottom)
+		.saturating_add(style.padding.top)
+		.saturating_add(style.padding.bottom);
+
+	Rect::new(
+		x,
+		y,
+		rect.width.saturating_sub(horizontal),
+		rect.height.saturating_sub(vertical),
+	)
 }
 
 #[cfg(test)]
